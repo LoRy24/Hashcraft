98d1c86ba3b5c034ce2f5ee7375f2a35ba685d1f
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-08-08 21:49:11 +0200

    Initial commit

---
+++ Bootstrap/src/main/java/com/github/lory24/hashcraft/bootstrap/HashcraftLauncher.java
@@ -0,0 +1,26 @@
+package com.github.lory24.hashcraft.bootstrap;
+
+/**
+ * The HashcraftLauncher is the class that will start the Proxy software. The major feature of this class is to
+ * check for additional parameters and to verify that the user is using the required version (for now it's Java 18).
+ * If the program fails to run, it will print the error in the console.
+ */
+@SuppressWarnings("SpellCheckingInspection")
+public class HashcraftLauncher {
+
+    /**
+     * The main function of the launcher. This will do all the stuff explained in the documentation of the class.
+     * @param args The arguments of the "java -jar" command
+     */
+    public static void main(String[] args) {
+
+        // Check the Java version
+        if (Float.parseFloat(System.getProperty("java.class.version")) < 62.0f) {
+            System.out.println("[LAUNCH-ERROR] You have to run this program with Java 18 or above. Please check your installation and try again.");
+            return;
+        }
+
+        // Launch the server in a new thread with a custom name
+        // TODO
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+---
+
+<div style="display: flex; flex-direction: column; justify-content: center; align-items: center; width: 100%; min-height: 380px">
+    <img src="png/HashcraftLogo.png" style="max-width: 300px; margin-top: 50px; margin-bottom: 0" alt="Hashcraft Logo">
+    <h1 style="font-size: 42px; margin-top: 0; text-align: center; margin-bottom: 0">Hashcraft Minecraft Proxy</h1>
+    <h2 style="font-size: 14px; margin-top: 0; text-align: center; margin-bottom: 50px">Connect mutliple spigot servers in only one network</h2>
+</div>
+
+---

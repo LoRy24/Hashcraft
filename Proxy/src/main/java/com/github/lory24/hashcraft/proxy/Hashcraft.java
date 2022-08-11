775f474570c8034d3486e745af25a326c3e6ccd6
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-08-11 17:37:41 +0200

    Start working on the netty server

--- Proxy/src/main/java/com/github/lory24/hashcraft/proxy/Hashcraft.java
+++ Proxy/src/main/java/com/github/lory24/hashcraft/proxy/Hashcraft.java
@@ -1,10 +1,18 @@
 package com.github.lory24.hashcraft.proxy;
 
 import com.github.lory24.hashcraft.api.Proxy;
+import com.github.lory24.hashcraft.api.util.ProxyConfiguration;
+import com.github.lory24.hashcraft.proxy.impl.HashcraftProxyConfiguration;
 import com.github.lory24.hashcraft.proxy.logger.CustomLoggerPrintStream;
 import com.github.lory24.hashcraft.proxy.logger.HashcraftLogger;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import lombok.Getter;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 /**
@@ -22,6 +30,15 @@ public class Hashcraft extends Proxy {
     @Getter
     private final HashcraftLogger hashcraftLogger;
 
+    @Getter
+    private final File configFile;
+
+    /**
+     * The Hashcraft proxy configuration object. Contains all the settings of the proxy.
+     */
+    @Getter
+    private HashcraftProxyConfiguration hashcraftConfiguration;
+
     /**
      * Inject the values instanced in the HashcraftProxyStarter
      *
@@ -31,13 +48,48 @@ public class Hashcraft extends Proxy {
     public Hashcraft(CustomLoggerPrintStream customLoggerPrintStream, HashcraftLogger hashcraftLogger) {
         this.customLoggerPrintStream = customLoggerPrintStream;
         this.hashcraftLogger = hashcraftLogger;
+        this.configFile = new File("./config.yml");
     }
 
     /**
      * Protected function that will start the Proxy server.
      */
     protected void start() {
-        this.getLogger().info("Hello World!");
+
+        // Load the Proxy configuration
+        this.getLogger().info("Loading configuration... ");
+        this.hashcraftConfiguration = new HashcraftProxyConfiguration(this.configFile); // Instance the hashcraft configuration object
+        this.hashcraftConfiguration.loadConfiguration(); // Load data
+        this.getLogger().info("Configuration loaded! Starting the proxy");
+
+        // Launch the netty server
+        this.startListening();
+    }
+
+    /**
+     * Start all the listeners and run the server
+     */
+    private void startListening() {
+        // Define the groups
+        final EventLoopGroup bossGroup = new NioEventLoopGroup(), workerGroup = new NioEventLoopGroup();
+
+        // Set up the serverBootstrap and start listening
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap()
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true);
+
+            // Bind the server
+            serverBootstrap.bind(this.hashcraftConfiguration.getProxyPort()).sync().channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // Shutdown the groups
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 
     /**
@@ -47,5 +99,15 @@ public class Hashcraft extends Proxy {
     @Override
     public Logger getLogger() {
         return Hashcraft.this.hashcraftLogger; // Return the instanced logger obj
+    }
+
+    /**
+     * Return the instanced ProxyConfiguration object.
+     *
+     * @see ProxyConfiguration
+     */
+    @Override
+    public ProxyConfiguration getProxyConfiguration() {
+        return Hashcraft.this.hashcraftConfiguration; // Return the instanced proxy configuration obj
     }
 }
---
+++ Proxy/src/main/java/com/github/lory24/hashcraft/proxy/netty/HashcraftChannelInitializer.java
@@ -0,0 +1,16 @@
+package com.github.lory24.hashcraft.proxy.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+public class HashcraftChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    /**
+     * Initialize the channel
+     * @param socketChannel The future socket channel
+     */
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+    }
+}

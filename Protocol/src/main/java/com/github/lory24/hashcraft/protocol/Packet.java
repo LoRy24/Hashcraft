4bfbdbb6a132f01ff36b593070f55915ee7747d7
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-08-14 22:25:14 +0200

    Move I/O functions in another class

--- Protocol/src/main/java/com/github/lory24/hashcraft/protocol/Packet.java
+++ Protocol/src/main/java/com/github/lory24/hashcraft/protocol/Packet.java
@@ -1,33 +1,31 @@
 package com.github.lory24.hashcraft.protocol;
 
 import io.netty.buffer.ByteBuf;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * The packet class. This contains all the features that a packet need and some other utility functions.
  * @author LoRy24
  */
-public abstract class Packet {
+public abstract class Packet extends PacketUtils {
 
     /**
-     * Read a varint from a netty bytebuf
+     * Read the data from a bytebuf into the packet object.
      *
-     * @param buf The buffer from where this function should read the VarInt
-     * @return The varInt
+     * @param buf The buf from where to read the data
      */
-    public static int readVarInt(@NotNull final ByteBuf buf) {
-        // Define the current value and the current position integers
-        int value = 0, position = 0;
-        byte current; // The current byte
+    public abstract void read(final ByteBuf buf);
 
-        while (true) {
-            current = buf.readByte(); // Read the byte from the bytebuf
-            value |= (current & 0x7f) << position; // Apply the value
-            if ((current & 0x80) == 0) break; // Check if it's the last byte
-            position += 7; // Increase the position by 7 bits
-            if (position >= 32) throw new RuntimeException("VarInt is too big"); // Trow an exception if the value is too big
-        }
+    /**
+     * Write the data stored in the packet object into a netty bytebuf
+     *
+     * @param buf The buf where to write the data
+     */
+    public abstract void write(final ByteBuf buf);
 
-        return value;
-    }
+    /**
+     * This function will be executed on the packet to handle it.
+     *
+     * @param packetHandler The current packet handler. Can be used to handle the packet in the current contex
+     */
+    public abstract void handle(final AbstractPacketHandler packetHandler);
 }
---
+++ Protocol/src/main/java/com/github/lory24/hashcraft/protocol/PacketUtils.java
@@ -0,0 +1,95 @@
+package com.github.lory24.hashcraft.protocol;
+
+import io.netty.buffer.ByteBuf;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Some utils for writing and reading data form a netty bytebuf
+ */
+public class PacketUtils {
+
+    /**
+     * Read a varint from a netty bytebuf
+     *
+     * @param buf The buffer from where this function should read the VarInt
+     * @return The varInt
+     */
+    public static int readVarInt(@NotNull final ByteBuf buf) {
+        // Define the current value and the current position integers
+        int value = 0, position = 0;
+        byte current; // The current byte
+
+        while (true) {
+            current = buf.readByte(); // Read the byte from the bytebuf
+            value |= (current & 0x7f) << position; // Apply the value
+            if ((current & 0x80) == 0) break; // Check if it's the last byte
+            position += 7; // Increase the position by 7 bits
+            if (position >= 32) throw new RuntimeException("VarInt is too big"); // Trow an exception if the value is too big
+        }
+
+        return value;
+    }
+
+    /**
+     * Write a varint value into the netty buffer.
+     *
+     * @param value The value that should be written in the buffer
+     * @param byteBuf The buffer where to put the data
+     */
+    public static void writeVarInt(int value, ByteBuf byteBuf) {
+
+        // "Infinite" loop
+        while (true) {
+
+            // Check if it is the last byte to write
+            if ((value & ~0x7F) == 0) {
+                byteBuf.writeByte(value);
+                return;
+            }
+
+            // Write the byte
+            byteBuf.writeByte((value & 0x7F) | 0x80);
+
+            // Note: >>> means that the sign bit is shifted with the rest of the number rather than being left alone
+            value >>>= 7;
+        }
+    }
+
+    /**
+     * Write a string into a buffer.
+     *
+     * @param byteBuf Where to write the string
+     * @param value The string to write in the buffer
+     */
+    public static void writeString(@NotNull String value, ByteBuf byteBuf) throws Exception {
+
+        // Check if the string is too long
+        if (value.getBytes(StandardCharsets.UTF_8).length > 32767) throw new Exception("String is too long!");
+
+        // Write the size of the string
+        writeVarInt(value.getBytes(StandardCharsets.UTF_8).length, byteBuf);
+
+        // Write the string bytes
+        byteBuf.writeBytes(value.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * Read a string from a buffer
+     */
+    @NotNull
+    @Contract("_ -> new")
+    public static String readString(ByteBuf buf) {
+        // Read the size of the string
+        int stringSize = readVarInt(buf);
+
+        // Read the string bytes
+        byte[] stringBytes = new byte[stringSize];
+        buf.readBytes(stringBytes);
+
+        // Return the new string
+        return new String(stringBytes, StandardCharsets.UTF_8);
+    }
+}

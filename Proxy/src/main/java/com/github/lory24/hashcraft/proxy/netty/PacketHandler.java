0072042e24d249a44032f05060cdfa1376315cd2
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-08-15 15:55:30 +0200

    Add an handle function in the PacketHandler impl

--- Proxy/src/main/java/com/github/lory24/hashcraft/proxy/netty/PacketHandler.java
+++ Proxy/src/main/java/com/github/lory24/hashcraft/proxy/netty/PacketHandler.java
@@ -2,21 +2,26 @@ package com.github.lory24.hashcraft.proxy.netty;
 
 import com.github.lory24.hashcraft.protocol.AbstractPacketHandler;
 import com.github.lory24.hashcraft.protocol.PacketWrapper;
-import com.github.lory24.hashcraft.protocol.packet.HandshakePacket;
 
 /**
  * This class will be exted by the packet handlers. The packet handlers are classes that are designed to do things
  * when a packet has been received by the proxy. With these functions, the handler will be able to process the packets
  * and do some other things.
  */
+@SuppressWarnings("unused")
 public class PacketHandler extends AbstractPacketHandler {
 
     /**
      * This function will return if the HashcraftProxyHandler should handle the packet
      */
-    public boolean shouldHandle(PacketWrapper packetWrapper) {
+    public boolean shouldHandle(final PacketWrapper packetWrapper) {
         return true;
     }
+
+    /**
+     * This function will handle a PacketWrapper
+     */
+    public void handle(final PacketWrapper packetWrapper) {}
 
     /**
      * This function will set the channel wrapper into the AbstractPacketHandler. This function should be called by the

55c44b6475345a8b5d9548759ab15a1f564dd7e1
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-08-15 15:03:00 +0200

    Add legacy handshake packet

--- Protocol/src/main/java/com/github/lory24/hashcraft/protocol/AbstractPacketHandler.java
+++ Protocol/src/main/java/com/github/lory24/hashcraft/protocol/AbstractPacketHandler.java
@@ -1,11 +1,13 @@
 package com.github.lory24.hashcraft.protocol;
 
 import com.github.lory24.hashcraft.protocol.packet.HandshakePacket;
+import com.github.lory24.hashcraft.protocol.packet.LegacyHandshakePacket;
 import com.github.lory24.hashcraft.protocol.packet.LegacyPingPacket;
 
 /**
  * The abstract packet handler. Contains function to handle each packet
  */
+@SuppressWarnings("RedundantThrows")
 public abstract class AbstractPacketHandler {
 
     /**
@@ -16,9 +18,16 @@ public abstract class AbstractPacketHandler {
     public void handle(final HandshakePacket handshakePacket) throws Exception {}
 
     /**
-     * This function will handle the handshake packet.
+     * This function will handle the legacy ping packet.
      *
      * @param legacyPingPacket The legacy ping packet
      */
     public void handle(final LegacyPingPacket legacyPingPacket) throws Exception {}
+
+    /**
+     * This function will handle the legacy handshake packet.
+     *
+     * @param legacyHandshakePacket The legacy handshake packet
+     */
+    public void handle(final LegacyHandshakePacket legacyHandshakePacket) throws Exception {}
 }

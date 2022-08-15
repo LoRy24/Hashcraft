e9ba4843299f510204ac5709f966b77eb1ba9288
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-08-15 13:51:42 +0200

    Create the legacy ping packet

---
+++ Protocol/src/main/java/com/github/lory24/hashcraft/protocol/packet/LegacyPingPacket.java
@@ -0,0 +1,47 @@
+package com.github.lory24.hashcraft.protocol.packet;
+
+import com.github.lory24.hashcraft.protocol.AbstractPacketHandler;
+import com.github.lory24.hashcraft.protocol.Packet;
+import io.netty.buffer.ByteBuf;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+@RequiredArgsConstructor
+public class LegacyPingPacket extends Packet {
+
+    /**
+     * If the connection is from version 1.5
+     */
+    private final boolean minecraftVersion1_5;
+
+    /**
+     * Read the data from a bytebuf into the packet object.
+     *
+     * @param buf The buf from where to read the data
+     */
+    @Override
+    public void read(ByteBuf buf) {
+
+    }
+
+    /**
+     * Write the data stored in the packet object into a netty bytebuf
+     *
+     * @param buf The buf where to write the data
+     */
+    @Override
+    public void write(ByteBuf buf) {
+
+    }
+
+    /**
+     * This function will be executed on the packet to handle it.
+     *
+     * @param packetHandler The current packet handler. Can be used to handle the packet in the current contex
+     */
+    @Override
+    public void handle(@NotNull AbstractPacketHandler packetHandler) throws Exception {
+        // Handle with the packetHandler
+        packetHandler.handle(this);
+    }
+}

5a57c8f0eff0c592cd6e96468b072962823516f3
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-08-19 16:29:33 +0200

    Create two internal proxy plugin's fields

--- Api/src/main/java/com/github/lory24/hashcraft/api/plugin/ProxyPlugin.java
+++ Api/src/main/java/com/github/lory24/hashcraft/api/plugin/ProxyPlugin.java
@@ -1,11 +1,27 @@
 package com.github.lory24.hashcraft.api.plugin;
 
+import com.github.lory24.hashcraft.api.Proxy;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * This class will be extended by the main classes of a plugin.
  *
  * @author LoRy24
  */
 public abstract class ProxyPlugin {
+
+    /**
+     * The plugin's description (the infos stored in the plugin.yml file). Updated when loading the plugin
+     */
+    @Getter @Setter
+    private PluginsDescription description;
+
+    /**
+     * A proxyserver reference
+     */
+    @Getter
+    private final Proxy proxy = Proxy.getInstance();
 
     /**
      * This function will be called when the plugin enables. It will be called on a new thread by the Hashcraft plugins'
